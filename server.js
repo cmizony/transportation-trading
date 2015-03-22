20af97a34eb720de51bd7bf223a557f77ae5af99
cmizony <cmizony@gmail.com>
2015-03-22 15:29:44 -0700

    Add initial skeleton project

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(__dirname));
+
+var server = app.listen(app.get('port'));
+
+console.log('Datatable tool app listening localhost at port ' + app.get('port'));
